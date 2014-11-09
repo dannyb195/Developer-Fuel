e791c66bc9e4ad3605122bff6df1b4fc6e1a793a
Dan Beil <dan@alleyinteractive.com>
2014-11-08 18:28:13 -0600

    slightly better js / ajax

--- scripts/df-scripts.js
+++ scripts/df-scripts.js
@@ -1,46 +1,40 @@
 ( function( $ ) {
 	$devFuel = $( '#dev-fuel' );
 	var lat,
-		lon;
+		lon,
+		geoError = 'Geolocation is not supported by this browser OR you have blocked your browser for using this feature, please allow your browser to use your location.';
 
 	function devFuelgetLocation() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(devFuelshowPosition);
+		if ( ! navigator.geolocation ) {
+			$( '#df-status' ).html( geoError );
 		} else {
-			$( '#df-status' ).html( 'Geolocation is not supported by this browser.' );
-		}
+			navigator.geolocation.getCurrentPosition( function( position ) {
+				if ( 'object' === typeof position ) {
+					var lat = position.coords.latitude,
+						lon = position.coords.longitude;
+					// This does the ajax request
+					$.ajax( {
+						url: ajaxurl,
+						data: {
+							'action':'dev_fuel_ajax_request',
+							'lat' : lat,
+							'long' : lon,
+							'current_time' : moment().format( 'HHmm' ),
+						},
+						success:function( data ) {
+							// This outputs the result of the ajax request
+							url = data;
+							$devFuel.attr( 'src', url );
+							$devFuel.css( 'opacity', '1' );
+						},
+						error: function( errorThrown ){
+						}
+					} ); // end ajax
+				} // end if object
+			} ); // navigator.geolocation.getCurrentPosition
+		} // end else
 	} // end getLocation
 
-	function devFuelshowPosition( position ) {
-		if ( 'undefined' == typeof position ) {
-			console.log( position );
-			$( '#df-status' ).html( 'Geolocation is not supported by this browser OR you have blocked your browser for using this feature, please allow your browser to use your location.' );
-		}
-		if ( 'undefined' != typeof position ) {
-			var lat = position.coords.latitude,
-				lon = position.coords.longitude;
-			// This does the ajax request
-			$.ajax( {
-				url: ajaxurl,
-				data: {
-					'action':'dev_fuel_ajax_request',
-					'lat' : lat,
-					'long' : lon,
-					'current_time' : moment().format( 'HHmm' ),
-				},
-				success:function( data ) {
-					// This outputs the result of the ajax request
-					url = data;
-					$devFuel.attr( 'src', url );
-					$devFuel.css( 'opacity', '1' );
-				},
-				error: function( errorThrown ){
-				}
-			} );
-		}
-	} // end showPosition
-
 	devFuelgetLocation();
-	devFuelshowPosition();
 
 } )( jQuery );
